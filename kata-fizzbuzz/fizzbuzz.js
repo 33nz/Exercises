0b959216cf075296cd8ff8abd5e3bf8b3640bc75
Jacob Wingate <jacob.wingate33@gmail.com>
2024-07-23 15:18:24 +1200

    add completed fizzbuzz challenge

--- kata-fizzbuzz/fizzbuzz.js
+++ kata-fizzbuzz/fizzbuzz.js
@@ -1,3 +1,33 @@
 const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const arr2 = [12, 22, 45, 47, 50, 63, 7]
 const arr3 = [17, -12, 'hello', 5, 23, 60, -7, null, 13]
+
+// multiple of 3 replaced with "Fizz"
+
+// multipple of 5 replaced with "Buzz"
+
+// multiple of 3 & r 5 replaced with "FizzBuzz"
+
+// non-number replaced with bonk
+
+function fizzBuzz(arr) {
+  return arr.map((x) => {
+    if (typeof x !== 'number' || isNaN(x)) {
+      return 'Bonk'
+    } else if (x % 15 === 0) {
+      return 'FizzBuzz'
+    } else if (x % 5 === 0) {
+      return 'Buzz'
+    } else if (x % 3 === 0) {
+      return 'Fizz'
+    } else if (x === NaN) {
+      return 'Bonk'
+    } else {
+      return x
+    }
+  })
+}
+
+console.log(fizzBuzz(arr1))
+console.log(fizzBuzz(arr2))
+console.log(fizzBuzz(arr3))
