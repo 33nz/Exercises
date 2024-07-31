692af9220dcbeb984545b21da3cb3f3b4807e8d3
Jacob Wingate <jacob.wingate33@gmail.com>
2024-07-31 14:40:29 +1200

    add code so far

--- kata-bowling/game.js
+++ kata-bowling/game.js
@@ -12,23 +12,32 @@
 //   [8, 1],
 // ]
 // Score 71 (with spares):
-const frames = [
-  [6, 1],
-  [4, 0],
-  [6, 4],
-  [2, 7],
-  [3, 5],
-  [5, 0],
-  [5, 5],
-  [0, 0],
-  [1, 6],
-  [7, 2],
-]
+// const frames = [
+//   [6, 1],
+//   [4, 0],
+//   [6, 4],
+//   [2, 7],
+//   [3, 5],
+//   [5, 0],
+//   [5, 5],
+//   [0, 0],
+//   [1, 6],
+//   [7, 2],
+// ]
 // Score 104 (with spares and strikes):
 // const frames = [
-//   [6, 4], [8, 0], [10, 0], [2, 7], [5, 5], [4, 0], [10, 0], [2, 1], [2, 6], [4, 4]
+//   [6, 4],
+//   [8, 0],
+//   [10, 0],
+//   [2, 7],
+//   [5, 5],
+//   [4, 0],
+//   [10, 0],
+//   [2, 1],
+//   [2, 6],
+//   [4, 4],
 // ]
-//
+
 // Score 119 (with spares, strikes and a double strike):
 // const frames = [
 //   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]
@@ -36,61 +45,102 @@ const frames = [
 //
 // Score 141 (includes a strike on the last frame):
 // const frames = [
-//   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [10, 10, 10]
+//   [1, 2],
+//   [6, 4],
+//   [5, 4],
+//   [10, 0],
+//   [7, 2],
+//   [10, 0],
+//   [10, 0],
+//   [5, 2],
+//   [7, 0],
+//   [10, 10, 10],
 // ]
 //
 // Score 300 (perfect game):
-// const frames = [
-//   [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 10, 10]
-// ]
+const frames = [
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 10, 10],
+]
 
-function addScore(frames) {
-  let totalScore = 0
+console.log(scoreBowling(frames))
 
-  frames.forEach((frame, index) => {
-    let frameScore = checkStrike(frames, frame, index)
-    if (frameScore < 10) {
-      // if no strike
-      frameScore = frame[0] + frame[1]
-      frameScore = checkSpares(frameScore, frame, index)
+function scoreBowling(frames) {
+  let score = 0
+
+  const evaluatedFrames = evaluateFrames(frames)
+
+  // handle first 9 frames
+  for (let i = 0; i < 9; i++) {
+    const currentFrame = evaluatedFrames[i]
+    const nextFrame = evaluatedFrames[i + 1]
+
+    score += currentFrame.frame[0] + currentFrame.frame[1]
+
+    if (currentFrame.isSpare) {
+      score += nextFrame.frame[0]
+    } else if (currentFrame.isStrike) {
+      score += getNextTwoBowlsValue(evaluatedFrames, i)
     }
-    totalScore += frameScore
-  })
-  return totalScore
-}
-
-console.log(addScore(frames))
-
-function checkSpares(frameScore, frames, index) {
-  if (frameScore === 10) {
-    frameScore += frames[index + 1][0] // adds score from next ball
   }
-  return frameScore
+  // handle last frame
+
+  const lastFrame = evaluatedFrames[evaluatedFrames.length - 1]
+  score += handleLastFrame(lastFrame)
+
+  return score
 }
 
-function checkStrike(frames, frame, index) {
-  if (frame[0] === 10) {
-    const nextFrameIndex = index + 1
-    const thirdFrameIndex = index + 2
-    let frameScore = 10
-    // deal with double strike
-    if (index === 9) {
-      frameScore = lastFrame(frame, frameScore)
-    } else if (frames[nextFrameIndex][0] === 10) {
-      frameScore += frames[nextFrameIndex][0] + frames[thirdFrameIndex][0]
+function handleLastFrame(lastFrame) {
+  let score = 0
+  if (lastFrame.isSpare) {
+    score += 10 + lastFrame.frame[2]
+  } else if (lastFrame.isStrike) {
+    score += 10 + lastFrame[1] + lastFrame[2]
+  } else {
+    score += lastFrame.frame[0] + lastFrame.frame[1]
+  }
+  return score
+}
+
+function getNextTwoBowlsValue(evaluatedFrames, currentIndex) {
+  let bowls = [...evaluatedFrames[currentIndex + 1].frame]
+
+  if (evaluatedFrames[currentIndex + 1].isStrike) {
+    if (currentIndex === 8) {
+      bowls.pop()
     } else {
-      frameScore += frames[nextFrameIndex][0] + frames[thirdFrameIndex][1]
+      bowls[1] = evaluatedFrames[currentIndex + 2].frame[0]
     }
-    return frameScore
-  } else return 0
+  }
+  return bowls[0] + bowls[1]
 }
 
-function lastFrame(frame, frameScore) {
-  const secondBall = frame[1]
-  const thirdBall = frame[2]
+function evaluateFrames(frames) {
+  const evaluated = []
 
-  if (secondBall === 10) {
-    frameScore += 2 * secondBall + thirdBall * 3
-  } else frameScore += 2 * secondBall + thirdBall * 2
-  return frameScore
+  for (let frame of frames) {
+    evaluated.push({
+      frame: frame,
+      isStrike: isStrike(frame),
+      isSpare: isSpare(frame),
+    })
+  }
+  return evaluated
+}
+
+function isStrike(frame) {
+  return frame[0] === 10
+}
+
+function isSpare(frame) {
+  return frame[0] + frame[1] === 10 && frame[0] !== 10
 }
