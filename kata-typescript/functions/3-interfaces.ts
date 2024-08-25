8ef0d08175f3690822267f1f1c9663d019223358
Jacob Wingate <jacob.wingate33@gmail.com>
2024-08-25 14:26:45 +0200

    add work so far

--- kata-typescript/functions/3-interfaces.ts
+++ kata-typescript/functions/3-interfaces.ts
@@ -34,7 +34,9 @@ export function findTheBat(contacts: Contact[]): string | null {
 
 // generateNewUser is passed a name
 // it should return a new (unverified) user object
-export function generateNewUser(name: Objectß): void {}
+export function generateNewUser(name: string): User {}
+{
+}
 
 // verifyUser is passed a User object and an email
 // it should return a new User object with an email and verified set to true
