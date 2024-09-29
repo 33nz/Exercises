51d1536d660ec305c3ffbef984a87f2ddc43cbcc
Jacob Wingate <jacob.wingate33@gmail.com>
2024-09-29 13:05:47 +0200

    add averageStudent score function

--- javascript-kata/src/4-gradebook/index.js
+++ javascript-kata/src/4-gradebook/index.js
@@ -57,7 +57,13 @@ function average(arr) {
 
 // 6. Complete the getAverage function that takes a name parameter and returns the average of that student’s testScores.
 // You can call the average function within this function.
-function getAverage(studentName) {}
+function getAverage(studentName) {
+  let studentScores = []
+  if (studentName in gradebook) {
+    studentScores = gradebook[studentName].testScores
+  }
+  return average(studentScores)
+}
 
 // 7. STRETCH MATERIAL
 // If you wish to attempt this stretch material, don't forget to remove the "skip" part of the last test in the index.test.js file in this folder
