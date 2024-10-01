e00d5ed0ce61cbc6a5961ec96e665cd069e6cf47
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-01 15:09:57 +0200

    add varables and arrays functions

--- javascript-kata/src/1-variables-and-arrays/index.js
+++ javascript-kata/src/1-variables-and-arrays/index.js
@@ -7,7 +7,9 @@ let members
 // assign values to each of these variables to pass the tests. The test descriptions will guide you.
 secretNumber = 7
 // write your code below this line---------------------------------
-
+password = 'just open the door'
+allowedIn = true
+members = ['Tui', 'Mana', 'Jeremy', 'Tane']
 // write your code above this line----------------------------------
 // -----------------------------------------------------------------
 export { secretNumber, password, allowedIn, members }
