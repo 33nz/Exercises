759c793877ca83ee73410f656e04b445054d374c
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-01 12:40:22 +0200

    add remove words from a string function

--- javascript-kata/src/5-builtin-methods/deBee.js
+++ javascript-kata/src/5-builtin-methods/deBee.js
@@ -2,7 +2,12 @@
 // You will need to split the string into individual words before you filter out all those annoying buzzing sounds!
 // It might be helpful to look at the deBee tests in the index.test.js file or use console.log inside of the function.
 
-function deBee(sentence) {}
+function deBee(sentence) {
+  return sentence
+    .split(' ')
+    .filter((word) => !word.toLowerCase().includes('buzz'))
+    .join(' ')
+}
 
 // write your code above this line----------------------------------
 // -----------------------------------------------------------------
