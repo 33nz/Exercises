59d5f7f97fa4d26e46822c2c5e293ba2339227ee
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-01 12:15:38 +0200

    add capitalise every word in a strind fucntion

--- javascript-kata/src/5-builtin-methods/capitalize.js
+++ javascript-kata/src/5-builtin-methods/capitalize.js
@@ -2,7 +2,12 @@
 // It needs to .map() over every word in that sentence and Capitalize The First Letter Of Every Word.
 // The function should return the modified sentence as a string.
 
-function capitalize(string) {}
+function capitalize(string) {
+  return string
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
+}
 
 // write your code above this line----------------------------------
 // -----------------------------------------------------------------
