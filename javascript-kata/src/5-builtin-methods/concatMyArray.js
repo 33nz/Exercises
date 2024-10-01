765da3e8c13759bd49d1c4f5dc6417c360c03808
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-01 11:51:04 +0200

    remove buggy js-kata

--- javascript-kata/src/5-builtin-methods/concatMyArray.js
+++
@@ -1,8 +0,0 @@
-// This function takes an array of individual strings
-// and then will need to .join() them together to return one string.
-
-function concatMyArray(arrayOfStrings) {}
-
-// write your code above this line----------------------------------
-// -----------------------------------------------------------------
-export { concatMyArray }
