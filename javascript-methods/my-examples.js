0e42dfcd345284db07716d9119ff080f71ca0fbe
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-02 20:33:34 +0200

    add map function example

--- javascript-methods/my-examples.js
+++ javascript-methods/my-examples.js
@@ -1,1 +1,10 @@
-// Write your two examples here
+// .map() will take an array and manipulate every element of the array to be something else
+
+let arr = [12, 34, 67, 864]
+
+function multiplyArr(arr) {
+  return arr.map((x) => x * 369)
+}
+
+const exampleResult = multiplyArr(arr)
+console.log(exampleResult)
