136467493d817ac9dd60c6bf55a6a6d97d801f97
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-02 20:21:01 +0200

    fix aotearoa typo

--- javascript-methods/string-methods.js
+++ javascript-methods/string-methods.js
@@ -1,22 +1,22 @@
 //  .length
-// We can use `.length` to get the length of a string. 
+// We can use `.length` to get the length of a string.
 // It returns an integer of how many characters are in the string. This includes spaces.
 // Example: length('Aotearoa') should return 8.
 // Example: length('New Zealand') should return 11.
 const str = 'Aotearoa'
 
-function length (str) {
+function length(str) {
   return str.length
 }
 
 // .toLowerCase() & .toUpperCase()
-// Sometimes with strings we want to write a function that changes the casing of letters in strings. 
-// This is when we want the string to be consistent while assigned to a variable but have certain situations like headings, links etc that the casing needs to change. 
-// Here's an example that uses both. 
+// Sometimes with strings we want to write a function that changes the casing of letters in strings.
+// This is when we want the string to be consistent while assigned to a variable but have certain situations like headings, links etc that the casing needs to change.
+// Here's an example that uses both.
 // Example: toCase('James') should return 'james-JAMES'.
 const aName = 'James'
 
-function toCase (name) {
+function toCase(name) {
   const lower = name.toLowerCase()
   const upper = name.toUpperCase()
   return lower + '-' + upper
@@ -26,14 +26,14 @@ function toCase (name) {
 // When we want to access a particular part of a string we can use `.charAt()` to get the character at a specified position.
 // It returns the character that matches that position in the string.
 
-// In this case we are using `.charAt()` to get the first character in each string to then return an acronym made from all three strings. 
-// Example: acronym('Dev', 'Academy', ) should return 'DAA'.
+// In this case we are using `.charAt()` to get the first character in each string to then return an acronym made from all three strings.
+// Example: acronym('Dev', 'Academy', 'Aotearoa') should return 'DAA'.
 // Example: acronym('New', 'Zealand', 'Dollar') should return 'NZD'.
 const str1 = 'Dev'
 const str2 = 'Academy'
 const str3 = 'Aotearoa'
 
-function acronym (str1, str2, str3) {
+function acronym(str1, str2, str3) {
   const one = str1.charAt(0)
   const two = str2.charAt(0)
   const three = str3.charAt(0)
