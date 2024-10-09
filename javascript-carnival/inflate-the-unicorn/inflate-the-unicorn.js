8a68468db0838130f1dce8d99b0cab529fca11f0
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-09 20:30:25 +0200

    add unicornElements and images array

--- javascript-carnival/inflate-the-unicorn/inflate-the-unicorn.js
+++ javascript-carnival/inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,16 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+let unicornElements = document.getElementsByClassName('inflate-an-image')
+
+let images = [
+  './images/unicorn-0.png',
+  './images/unicorn-1.png',
+  './images/unicorn-2.png',
+  './images/unicorn-3.png',
+]
+
+let imageIndex = 0
