1fc1fffaacd13a6dcef3db4ccd8cd38404785070
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-10 20:55:31 +0200

    add inflate the unicorn code with getElementByClassName, addEventListener on Unicorn and loop

--- javascript-carnival/inflate-the-unicorn/inflate-the-unicorn.js
+++ javascript-carnival/inflate-the-unicorn/inflate-the-unicorn.js
@@ -6,11 +6,22 @@ console.log('Inflate The Unicorn!')
 
 let unicornElements = document.getElementsByClassName('inflate-an-image')
 
-let images = [
+let imageIndex = 0
+
+let imageFiles = [
   './images/unicorn-0.png',
   './images/unicorn-1.png',
   './images/unicorn-2.png',
   './images/unicorn-3.png',
 ]
 
-let imageIndex = 0
+for (let i = 0; i < unicornElements.length; i++) {
+  let unicorn = unicornElements[i]
+
+  unicorn.addEventListener('click', function () {
+    console.log('Clicked Unicorn: ', unicorn)
+    unicorn.src = imageFiles[imageIndex]
+
+    imageIndex = (imageIndex + 1) % imageFiles.length
+  })
+}
