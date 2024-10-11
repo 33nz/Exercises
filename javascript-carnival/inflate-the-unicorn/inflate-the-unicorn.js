f1046921fe8c0d5a82f3d708ca5833778df54f66
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-11 20:33:55 +0200

    add modulus wrap pesudo code

--- javascript-carnival/inflate-the-unicorn/inflate-the-unicorn.js
+++ javascript-carnival/inflate-the-unicorn/inflate-the-unicorn.js
@@ -25,3 +25,10 @@ for (let i = 0; i < unicornElements.length; i++) {
     imageIndex = (imageIndex + 1) % imageFiles.length
   })
 }
+
+// The imageIndex will go through the following values on successive clicks:
+// If imageIndex = 0: (0 + 1) % 4 = 1.
+// If imageIndex = 1: (1 + 1) % 4 = 2.
+// If imageIndex = 2: (2 + 1) % 4 = 3.
+// If imageIndex = 3: (3 + 1) % 4 = 0 (wraps back to 0).
+// This is how the "wrap around" works, letting the image index reset to 0 once it reaches the end of the array.
