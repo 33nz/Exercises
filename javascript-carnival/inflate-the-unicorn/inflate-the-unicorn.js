c8472ba86fca951a97d5f61264edbc30dc336646
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-13 12:45:39 +0200

    remove code for redo/practice

--- javascript-carnival/inflate-the-unicorn/inflate-the-unicorn.js
+++ javascript-carnival/inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,25 +1,3 @@
 // -    -   -   -   -  //
 // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
-
-console.log('Inflate The Unicorn!')
-
-let unicornElements = document.getElementsByClassName('inflate-an-image')
-
-let imageFiles = [
-  './images/unicorn-0.png',
-  './images/unicorn-1.png',
-  './images/unicorn-2.png',
-  './images/unicorn-3.png',
-]
-
-let imageIndex = 0
-
-for (let i = 0; i < unicornElements.length; i++) {
-  let unicorn = unicornElements[i]
-
-  unicorn.addEventListener('click', function () {
-    unicorn.src = imageFiles[imageIndex]
-    imageIndex = (imageIndex + 1) % imageFiles.length
-  })
-}
