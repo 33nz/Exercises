788dfb4d81f404848d546654e185c9a7370b8fe3
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-13 22:28:59 +0200

    add first attempt at mole code

--- javascript-carnival/whack-a-mole/whack-a-mole.js
+++ javascript-carnival/whack-a-mole/whack-a-mole.js
@@ -9,6 +9,22 @@ let cells = document.getElementsByClassName('cell')
 console.log(cells)
 console.log('cell count:' + cells.length)
 
-let randomIndex = Math.floor(Math.random() * cells.length)
-
 console.log(randomIndex)
+
+function popMole() {
+  for (let cell of cells) {
+    if (cell.querySelector('img')) {
+      cell.removeChild(cell.querySelector('img'))
+    }
+  }
+
+  const img = document.createElement('img')
+  img.src = './whack-a-mole/mole.png'
+  img.alt = 'Mole'
+
+  let randomIndex = Math.floor(Math.random() * cells.length)
+
+  let cell = cells[randomIndex]
+
+  cell.appendChild(img)
+}
