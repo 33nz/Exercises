d8c239ada35f97c731a95dfa19c8cad10e29ae7b
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-13 13:07:42 +0200

    add cells and randomIndex code

--- javascript-carnival/whack-a-mole/whack-a-mole.js
+++ javascript-carnival/whack-a-mole/whack-a-mole.js
@@ -1,5 +1,14 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+let cells = document.getElementsByClassName('cell')
+
+console.log(cells)
+console.log('cell count:' + cells.length)
+
+let randomIndex = Math.floor(Math.random() * cells.length)
+
+console.log(randomIndex)
