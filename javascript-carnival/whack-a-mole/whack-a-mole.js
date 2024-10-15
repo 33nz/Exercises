4b5d52133a76b5ce83d6064ede4a5bea5b74127e
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-15 20:22:47 +0200

    fix img bug when same cell selected

--- javascript-carnival/whack-a-mole/whack-a-mole.js
+++ javascript-carnival/whack-a-mole/whack-a-mole.js
@@ -8,30 +8,28 @@ console.log('Whack-a-Mole!')
 
 let cellsArray = document.getElementsByClassName('cell')
 
-let randomIndex = getRandomNumber(0, cellsArray.length)
-let randomCell = cellsArray[randomIndex]
+function placeMole() {
+  let randomIndex = getRandomNumber(0, cellsArray.length)
+  let randomCell = cellsArray[randomIndex]
 
-const img = document.createElement('img')
-img.src = './mole.png'
-img.alt = 'Mole'
-img.width = 75
-img.heigh = 75
+  const img = document.createElement('img')
+  img.src = './mole.png'
+  img.alt = 'Mole'
+  img.width = 75
+  img.heigh = 75
 
-randomCell.appendChild(img)
+  img.onclick = whackedMole
+
+  randomCell.appendChild(img)
+}
 
 function whackedMole() {
   this.parentNode.removeChild(this)
 
-  let newIndex = getRandomNumber(0, cellsArray.length)
-
-  let newCell = cellsArray[newIndex]
-
-  newCell.appendChild(img)
-
   const whackAudio = document.createElement('audio')
   whackAudio.src = './whack-audio.wav'
-
   whackAudio.play()
-}
 
-img.onclick = whackedMole
+  placeMole()
+}
+placeMole()
