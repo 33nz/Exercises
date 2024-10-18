384826e5314373afb52b505d8ba7be101c470f94
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-18 20:18:55 +0200

    add leftKey to change index down

--- javascript-carnival/dress-the-clown/dress-the-clown.js
+++ javascript-carnival/dress-the-clown/dress-the-clown.js
@@ -4,12 +4,20 @@
 
 console.log('Dress The Clown!')
 
-let headIndex = 2
+let headIndex = 0
+let bodyIndex = 0
+let shoeIndex = 0
 
 function changeClownHead() {
   const head = document.getElementById('head')
   let headSrc = './images/head' + headIndex + '.png'
   head.src = headSrc
+}
+
+function changeBody() {
+  const body = document.getElementById('body')
+  let bodySrc = './images/body' + bodyIndex + '.png'
+  body.src = bodySrc
 }
 
 document.onkeydown = checkKey
@@ -19,6 +27,15 @@ function checkKey(e) {
 
   if (e.keyCode == '39') {
     headIndex++
+    if (headIndex > 5) {
+      headIndex = 0
+    }
+    changeClownHead()
+  } else if (e.keyCode == '37') {
+    headIndex--
+    if (headIndex < 0) {
+      headIndex = 5
+    }
     changeClownHead()
   }
 }
