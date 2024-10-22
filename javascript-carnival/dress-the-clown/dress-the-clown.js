cf06ac91ee4110bfe68679a8d86b0882770fba03
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-22 11:50:13 +0200

    add getPartName function and first half of check key for dom manipulation

--- javascript-carnival/dress-the-clown/dress-the-clown.js
+++ javascript-carnival/dress-the-clown/dress-the-clown.js
@@ -19,3 +19,35 @@ let indexes = [0, 0, 0]
 let head = document.getElementById('head')
 let body = document.getElementById('body')
 let shoes = document.getElementById('shoes')
+
+document.onkeydown(checkKey)
+
+function checkKey(e) {
+  // up arrow
+  if (e.keyCode == '38') {
+    partIndex--
+    if (partIndex < 0) partIndex = 2
+  }
+  // down arrow
+  else if (e.keyCode == '40') {
+    partIndex++
+    if (partIndex > 2) partIndex = 0
+  }
+  // decrease index of current part
+  else if (e.keyCode == '37') {
+    indexes[partIndex]--
+    if (indexes[partIndex] < 0) indexes[partIndex] = 5
+    elements[partIndex].src =
+      './images/' + getPartName(partIndex) + indexes[partIndex] + '.png'
+  }
+}
+
+function getPartName(partIndex) {
+  if (partIndex === 0) {
+    return 'head'
+  } else if (partIndex === 1) {
+    return 'body'
+  } else {
+    return 'shoes'
+  }
+}
