a7625b76f7238c1b4b9967e26d2f5b57fa5ebf56
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-22 11:26:31 +0200

    add partIndex and bodyElements array

--- javascript-carnival/dress-the-clown/dress-the-clown.js
+++ javascript-carnival/dress-the-clown/dress-the-clown.js
@@ -3,3 +3,11 @@
 // -    -   -   -   -  //
 
 import { save } from './screenshot.js'
+
+let partIndex = 0
+
+let elements = [
+  document.getElementById('head'),
+  document.getElementById('body'),
+  document.getElementById('shoes'),
+]
