ea3b70f461ef5948163315a9e3aba22f6edb4323
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-23 17:37:25 +0200

    add onclick function

--- tic-tac-toe/game.js
+++ tic-tac-toe/game.js
@@ -3,3 +3,9 @@ console.log('Welcome to Tic-Tac-Toe! Enjoy!')
 let cells = document.getElementsByTagName('td')
 
 console.log(cells)
+
+function sayHello() {
+  console.log('Hello')
+}
+
+cells[0].onclick = sayHello
