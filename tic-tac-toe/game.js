14924e3f0afd0f23fab362ee49b987f160f3c223
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-24 10:06:56 +0200

    add cellClicked() pseudocode

--- tic-tac-toe/game.js
+++ tic-tac-toe/game.js
@@ -12,6 +12,20 @@ function cellClicked(e) {
   let cell = e.target
   let index = Array.prototype.indexOf.call(cells, cell)
   console.log('I clicked on cell ' + (index + 1))
+
+  // create a variable for the cell clicked so I can do stuff with it
+
+  // if the cell is empty check its innerHTML property
+
+  // figure out which symbol to put in cell based on the noughtsTurn boolean
+
+  // put the symbol inside the cell by using innerHTML again
+
+  // check to see if the player won the game with that move (probably using a new function, like checkForWin() which I'll write later)
+
+  // if the game isn't over: switch to the other player using noughtsTurn again
+
+  // update the subtitle saying whose turn it is now
 }
 
 let noughtsTurn = true
