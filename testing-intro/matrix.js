04dcfe3a4a83814db9f647c74930a19b299086dc
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-24 21:48:50 +0200

    add getMatrix function wiht double loops and column/row creation

--- testing-intro/matrix.js
+++ testing-intro/matrix.js
@@ -3,8 +3,14 @@ export function getMatrix(n) {
   let matrix = []
 
   for (let i = 0; i < n; i++) {
-    matrix.push([])
+    let row = []
+
+    for (let j = 0; j < n; j++) {
+      row.push(0)
+    }
+    matrix.push(row)
   }
+  return matrix
 }
 
 export function updateMatrix(matrix, coords, value) {}
