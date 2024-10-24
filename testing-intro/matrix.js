bac351797396a5cdad170cf388db7c9ba4a896f1
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-24 21:43:08 +0200

    add getMatrix code so far

--- testing-intro/matrix.js
+++ testing-intro/matrix.js
@@ -1,4 +1,10 @@
 /* eslint-disable no-unused-vars */
-export function getMatrix(n) {}
+export function getMatrix(n) {
+  let matrix = []
+
+  for (let i = 0; i < n; i++) {
+    matrix.push([])
+  }
+}
 
 export function updateMatrix(matrix, coords, value) {}
