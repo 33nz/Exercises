482c7d896979dd387786a37c4c202ca08234719b
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-25 07:06:15 +0200

    add find function by seraching Object.keys

--- testing-intro/find.js
+++ testing-intro/find.js
@@ -1,2 +1,8 @@
 /* eslint-disable no-unused-vars */
-export default function find(arr, searchDetails) {}
+export default function find(arr, searchDetails) {
+  return arr.find((contact) =>
+    Object.keys(searchDetails).every(
+      (key) => contact[key] === searchDetails[key],
+    ),
+  )
+}
