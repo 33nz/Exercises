0d9dfeebf3e0f5fda2d640325d9e8de901fbe0dc
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-26 20:29:10 +0200

    add whereFuction using filter

--- testing-intro/where.js
+++ testing-intro/where.js
@@ -1,2 +1,8 @@
 /* eslint-disable no-unused-vars */
-export default function where(arr, searchDetails) {}
+export default function where(arr, searchDetails) {
+  return arr.filter((contact) => {
+    return Object.keys(searchDetails).every(
+      (key) => contact[key] === searchDetails[key],
+    )
+  })
+}
