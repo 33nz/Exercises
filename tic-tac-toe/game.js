6b369192ae9d06ef6905fc05bb969a55268be284
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-27 11:55:40 +0100

    add refactored change innerHTML, add toggleTurn() and start of checkWin()

--- tic-tac-toe/game.js
+++ tic-tac-toe/game.js
@@ -1,6 +1,7 @@
 console.log('Welcome to Tic-Tac-Toe! Enjoy!')
 
 let cells = document.getElementsByTagName('td')
+const subtitle = docuument.getElementById('subtitle')
 
 console.log(cells)
 
@@ -9,32 +10,23 @@ for (let i = 0; i < cells.length; i++) {
 }
 
 let noughtsTurn = true
+let gameIsOver
 
-function cellClicked(e) {
-  // create a variable for the cell clicked so I can do stuff with it
-  let cell = e.target
-  let index = Array.prototype.indexOf.call(cells, cell)
-  console.log('I clicked on cell ' + (index + 1))
-
-  // if the cell is empty check its innerHTML property
-
-  if (cell.innerHTML == '' && noughtsTurn) {
-    cell.innerHTML = 'O'
-  } else if (cell.innerHTML == '' && !noughtsTurn) {
-    cell.innerHTML = 'X'
-  } else return
-
+function toggleTurn() {
   noughtsTurn = !noughtsTurn
-
-  // figure out which symbol to put in cell based on the noughtsTurn boolean
-
-  // put the symbol inside the cell by using innerHTML again
-
-  // check to see if the player won the game with that move (probably using a new function, like checkForWin() which I'll write later)
-
-  // if the game isn't over: switch to the other player using noughtsTurn again
-
-  // update the subtitle saying whose turn it is now
 }
 
-let gameIsOver
+function cellClicked(e) {
+  // create a variable for the cell clicked
+  let cell = e.target
+  // if the cell is empty (check it's innerHTML property)
+  if (cell.innerHTMl == '') {
+    let sybmol = noughtsTurn ? 'O' : 'X'
+
+    cell.innerHTML = sybmol
+
+    subtitle.textContent = noughtsTurn
+      ? 'It is now Xs turn!!'
+      : 'It is now Os turn!!'
+  }
+}
